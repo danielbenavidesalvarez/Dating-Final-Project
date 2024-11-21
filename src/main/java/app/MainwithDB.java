ce3af529e327f8b076c9b8172108a93f7a8e5683
Daniel Benavides-Alvarez <daniel.benavidesalvarez@mail.utoronto.ca>
2024-11-21 16:04:47 -0500

    Initial copying of directory

--- src/main/java/app/MainwithDB.java
+++ src/main/java/app/MainwithDB.java
@@ -1,4 +1,10 @@
 package app;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupViewModel;
+import view.ViewManager;
+
 import java.awt.CardLayout;
 
 import javax.swing.JFrame;
@@ -6,5 +12,30 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 public class MainwithDB {
+    public static void main(String[] args) {
+        final JFrame application = new JFrame("Dating Application");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        final CardLayout cardLayout = new CardLayout();
+        final JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+
+        final ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        final LoginViewModel loginViewModel = new LoginViewModel();
+        final LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+        final SignupViewModel signupViewModel = new SignupViewModel();
+
+
+
+
+
+
+
+        application.pack();
+        application.setVisible(true);
+    }
 
 }
