cc4bd3577b155b488f443f08b48ee9b16b0b2bde
luiscc02 <luis.calderon@mail.utoronto.ca>
2024-12-01 17:12:06 -0500

    login signup interface adapter

--- src/main/java/interface_adapter/login/LoginState.java
+++ src/main/java/interface_adapter/login/LoginState.java
@@ -1,20 +1,35 @@
 package interface_adapter.login;
 
+/**
+ * The state for the Login View Model.
+ */
 public class LoginState {
-
     private String username = "";
     private String loginError;
     private String password = "";
 
-    public String getUsername(){return username; }
+    public String getUsername() {
+        return username;
+    }
 
-    public String getLoginError(){return loginError; }
+    public String getLoginError() {
+        return loginError;
+    }
 
-    public String getPassword() {return password; }
+    public String getPassword() {
+        return password;
+    }
 
-    public void setUsername(String username){ this.username = username; }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public void setLoginError(String loginError){ this.loginError = loginError; }
+    public void setLoginError(String usernameError) {
+        this.loginError = usernameError;
+    }
 
-    public void setPassword(String password){ this.password = password; }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
